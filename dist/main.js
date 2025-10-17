977da320bb200ecce9904fffb458e8abe69e6f58
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-10-17 15:02:04 -0300

    feat: create hasWinner function

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,31 @@ const gameSymbol = new GameSymbol("x");
 // DOM Elements
 const gameBoxes = document.querySelectorAll(".game__box");
 // Functions Declaration
+function hasWinner() {
+    const firstRow = Array.from(gameBoxes).filter((_, i) => i >= 0 && i <= 2);
+    const secondRow = Array.from(gameBoxes).filter((_, i) => i >= 3 && i <= 5);
+    const thirdRow = Array.from(gameBoxes).filter((_, i) => i >= 6 && i <= 8);
+    const firstDiagonal = Array.from(gameBoxes).filter((_, i) => i === 2 || i === 4 || i === 6);
+    const secondDiagonal = Array.from(gameBoxes).filter((_, i) => i === 0 || i === 4 || i === 8);
+    const hasAllSameSymbol = (row) => {
+        return row.every((e) => {
+            return (e.textContent === row[0].textContent && row[0].textContent.trim() !== "");
+        });
+    };
+    const hasFirstRowSameSymbol = hasAllSameSymbol(firstRow);
+    const hasSecondRowSameSymbol = hasAllSameSymbol(secondRow);
+    const hasThirdRowSameSymbol = hasAllSameSymbol(thirdRow);
+    const hasFirstDiagonalSameSymbol = hasAllSameSymbol(firstDiagonal);
+    const hasSecondDiagonalSameSymbol = hasAllSameSymbol(secondDiagonal);
+    const plays = [
+        hasFirstRowSameSymbol,
+        hasSecondRowSameSymbol,
+        hasThirdRowSameSymbol,
+        hasFirstDiagonalSameSymbol,
+        hasSecondDiagonalSameSymbol,
+    ];
+    return plays.some((result) => result);
+}
 function markGameBox(box) {
     const isBoxMarked = box.classList.contains("game__box--marked");
     if (!isBoxMarked) {
@@ -16,8 +41,10 @@ function markGameBox(box) {
 if (gameBoxes) {
     gameBoxes.forEach((box) => {
         box.addEventListener("click", (e) => {
-            const target = e.target;
-            markGameBox(target);
+            if (!hasWinner()) {
+                const target = e.target;
+                markGameBox(target);
+            }
         });
     });
 }
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -8,6 +8,44 @@ const gameSymbol = new GameSymbol("x");
 const gameBoxes = document.querySelectorAll(".game__box");
 
 // Functions Declaration
+function hasWinner(): boolean {
+  const firstRow = Array.from(gameBoxes).filter((_, i) => i >= 0 && i <= 2);
+  const secondRow = Array.from(gameBoxes).filter((_, i) => i >= 3 && i <= 5);
+  const thirdRow = Array.from(gameBoxes).filter((_, i) => i >= 6 && i <= 8);
+
+  const firstDiagonal = Array.from(gameBoxes).filter(
+    (_, i) => i === 2 || i === 4 || i === 6
+  );
+
+  const secondDiagonal = Array.from(gameBoxes).filter(
+    (_, i) => i === 0 || i === 4 || i === 8
+  );
+
+  const hasAllSameSymbol = (row: Element[]) => {
+    return row.every((e) => {
+      return (
+        e.textContent === row[0].textContent && row[0].textContent.trim() !== ""
+      );
+    });
+  };
+
+  const hasFirstRowSameSymbol = hasAllSameSymbol(firstRow);
+  const hasSecondRowSameSymbol = hasAllSameSymbol(secondRow);
+  const hasThirdRowSameSymbol = hasAllSameSymbol(thirdRow);
+  const hasFirstDiagonalSameSymbol = hasAllSameSymbol(firstDiagonal);
+  const hasSecondDiagonalSameSymbol = hasAllSameSymbol(secondDiagonal);
+
+  const plays = [
+    hasFirstRowSameSymbol,
+    hasSecondRowSameSymbol,
+    hasThirdRowSameSymbol,
+    hasFirstDiagonalSameSymbol,
+    hasSecondDiagonalSameSymbol,
+  ];
+
+  return plays.some((result) => result);
+}
+
 function markGameBox(box: HTMLElement): void {
   const isBoxMarked = box.classList.contains("game__box--marked");
 
@@ -21,8 +59,10 @@ function markGameBox(box: HTMLElement): void {
 if (gameBoxes) {
   gameBoxes.forEach((box) => {
     box.addEventListener("click", (e) => {
-      const target = e.target as HTMLElement;
-      markGameBox(target);
+      if (!hasWinner()) {
+        const target = e.target as HTMLElement;
+        markGameBox(target);
+      }
     });
   });
 }
